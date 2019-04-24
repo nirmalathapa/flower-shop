110bd9492257207b6799d676629e6759f2bcfb74
nirmala <nirmala@surf.net.np>
2019-04-24 15:33:40 +0200

    add data

---
+++ src/data.js
@@ -0,0 +1,111 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Wedding Flowers",
+    img: "img/product-1.png",
+    price: 10,
+    company: "Wedding flowers",
+    info:
+      "Flowers have always been a big feature at weddings, too. As an example, look to the royal flower bouquet in the wedding of Prince William, Duke of Cambridge, to Kate Middleton (now Catherine, Ducchess of Cambridge). Her flowers had very special meaning. The groom, too, wears a flower that appears in the bridal bouquet in his button-hole. This stems from the Medieval tradition of wearing his Lady’s colors, as a declaration of his love.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Express Love",
+    img: "img/product-2.png",
+    price: 16,
+    company: "SAMSUNG",
+    info:
+      "The rose has been a symbol of love, beauty, even war and politics from way back in time. The variety, color and even number of Roses carry symbolic meanings. The Rose is most popularly known as the flower of love, particularly Red Rose. Roses have been the most popular choice of flowers for the purpose of gifting across the world.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Birthday Boquet",
+    img: "img/product-3.png",
+    price: 8,
+    company: "htc",
+    info:
+      "Birthdays are a joyful time for everyone involved and planning how to celebrate should be as much fun as the celebration itself. If you need to mark an important time in someone's life, choosing the right celebration is important, especially if it is a particularly big birthday, a coming of age at 21 perhaps? Celebrations should always be surprises and should be joyful and colorful but how do manage to do this when you are apart.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Mix Flowers",
+    img: "img/product-4.png",
+    price: 18,
+    company: "htc",
+    info:
+      "Flowers are presented as gift in a bevy of occasions like weddings, festivals, funerals and anniversaries. Every sentiment is expressed in one form or another by these fragile blooms. While flowers gifted on specific occasions depend mostly on the season in which the occasion falls, ultimately it is the personality of the reciepent that should be the deciding factor in the choice of the flower gift.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Pink Love",
+    img: "img/product-5.png",
+    price: 24,
+    company: "htc",
+    info:
+      "There are a lot of variations of the pink rose. Over all, pink roses are used to convey gentle emotions such as admiration, joy and gratitude. Light pink rose blooms are indicative of sweetness and innocence. Deep pink rose blooms convey deep gratitude and appreciation. Pink roses also connote elegance and grace.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Mix of White and Red",
+    img: "img/product-6.png",
+    price: 17,
+    company: "apple",
+    info:
+      "Surprise is the key here. Again, seasonal flowers are the best bet. Options include combining with gourmet or fruit basket. As for gifting flowers to friends and relatives on the occasion of their anniversary, try something that fits in with their personalities. Whatever it is, make sure to add a personal touch to the flowers.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Give Happiness",
+    img: "img/product-7.png",
+    price: 30,
+    company: "apple",
+    info:
+      "Brightly colored flowers are a perfect way to introduce the new born to our world. Try sending some of the most jovial flower gift and lively bouquets to give a warm welcome to the little one.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Blue Orchid",
+    img: "img/product-8.png",
+    price: 2,
+    company: "apple",
+    info:
+      "Orchid is a symbol of the exotic beauty. It symbolizes refinement, thoughtfulness and mature charm. It also symbolizes proud and glorious femininity. It is easy to understand why people fall prey to the obsession with orchids. It is difficult to get close to a fresh plant in full bloom and not get smitten by its fascinating and all-too-rare unique looks. Today, more than ever, millions of people are devoted to planting and growing orchids, and the orchid remains a favorite amongst many as a precious gift to give to loved ones.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Wedding Flowers",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "rises are wonderful garden plants. The word Iris means rainbow. Irises come in many colors such as blue and purple, white and yellow, pink and orange, brown and red, and even black. Iris symbolizes eloquence. Purple iris is symbolic of wisdom and compliments. Blue iris symbolizes faith and hope. Yellow iris symbolizes passion while white iris symbolizes purity.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
