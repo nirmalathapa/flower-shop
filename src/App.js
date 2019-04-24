81ef07e97dab5fd385124be1b08cba34feacdd13
nirmala <nirmala@surf.net.np>
2019-04-24 15:35:29 +0200

    fix path for the entry of cart

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Cart from "./components/Cart";
+import NotFound from "./pages/NotFound";
+import Modal from "./components/Modal";
+import Details from "./pages/Details";
+import ProductList from "./pages/ProductList";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={NotFound} />
+        </Switch>
+        <Modal />
+      </React.Fragment>
     );
   }
 }
